956eea715893231dd6a3503f9d3365a351e4482c
Yuliya <37079631+YuliyaBiziuk@users.noreply.github.com>
2020-05-07 22:12:43 +0100

    Update 04-date-tasks.js

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(value) {
-  
+function parseDataFromRfc2822(/* value */) {
+  throw new Error('Not implemented');
 }
 
 
@@ -35,9 +35,10 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(value) {
-  let mydate = new Date(value);
-  return mydate.toDateString();
+function parseDataFromIso8601(/* value */) {
+  throw new Error('Not implemented');
+  /* const mydate = new Date(value);
+  return mydate.toDateString(); */
 }
 
 
@@ -55,9 +56,9 @@ function parseDataFromIso8601(value) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(date) {
-  return date % 4 == 0 &&
-    (date % 100 !== 0 || date % 400 === 0);
+function isLeapYear(/* date */) {
+  throw new Error('Not implemented');
+  /* return date % 4 === 0 && (date % 100 !== 0 || date % 400 === 0); */
 }
 
 
@@ -76,8 +77,8 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(startDate, endDate) {
-  
+function timeSpanToString(/* startDate, endDate */) {
+  throw new Error('Not implemented');
 }
 
 
